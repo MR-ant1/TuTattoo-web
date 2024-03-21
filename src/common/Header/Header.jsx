26e230c20c585623f7eda9223db9a78a83ffed19
antonio <antoniorc7@gmail.com>
2024-03-21 23:23:08 +0100

    feat: Navigation section for register and home pages in header created

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -1,11 +1,29 @@
 
+import { Navigator } from "../Navigator/Navigator"
 import "./Header.css"
 
 export const Header = () => {
 
+    const token = false
+
+    const logOut = () => {
+        
+    } //Adding register navigation to allow move from header. Included a token validation to show register and login or icon to profile and log out (future)
     return (
-        <div className="headerDesign">
-            
+        <div className = "headerDesign">
+            <Navigator title={"home"} sendTo={"/"} />
+                        
+            {token ? (
+                <div>  
+                    <Navigator title={"nickdelusuario"} sendTo={"/"} />
+                    <Navigator title={"log out"} onClick={() => logOut()} />
+                </div>
+            ) : (
+                <div>
+                    <Navigator title={"register"} sendTo={"/register"} />
+                </div>
+            )
+            }
         </div>
     )
 }
