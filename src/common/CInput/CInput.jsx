dfd32f97174483ae8053078e9dece536eabeed13
antonio <antoniorc7@gmail.com>
2024-03-22 09:34:32 +0100

    feat:inputs in register created. Next step is to make them work

---
+++ src/common/CInput/CInput.jsx
@@ -0,0 +1,16 @@
+
+import "./CInput.css"
+
+export const CInput = ({className, type, placeholder, name, value, onChangeFunction}) => {
+
+    return(
+        <input
+            className={className}
+            type={type}
+            placeholder={placeholder}
+            name={name}
+            value={value}
+            onChange={onChangeFunction}
+        />
+    )
+}
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,53 @@
 
+import { useState } from "react"
+import { CInput } from "../../common/CInput/CInput"
 import "./Register.css"
 
 export const Register = () => {
 
+const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: ""
+})
+ 
+const inputHandler = (e) => {
+    console.log(e.target.value)
+}
 return (
     <div className="registerDesign">
-        Register
+        <CInput
+            className={"inputDesign"}
+            type={"text"}
+            placeholder={"firstName"}
+            name={"firstName"}
+            value={user.firstName || ""}
+            onChangeFunction={(e) => inputHandler(e)}
+        />
+        <CInput
+            className={"inputDesign"}
+            type={"text"}
+            placeholder={"lastName"}
+            name={"lastName"}
+            value={user.lastName || ""}
+            onChangeFunction={(e) => inputHandler(e)}
+        />
+        <CInput
+            className={"inputDesign"}
+            type={"text"}
+            placeholder={"email"}
+            name={"email"}
+            value={user.email || ""}
+            onChangeFunction={(e) => inputHandler(e)}
+        />
+        <CInput
+            className={"inputDesign"}
+            type={"text"}
+            placeholder={"password"}
+            name={"password"}
+            value={user.password || ""}
+            onChangeFunction={(e) => inputHandler(e)}
+        />
     </div>
 )
 
